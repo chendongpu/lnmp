cb29f2f82491bb0649228ff6a124853e75669ae0
chendongpu <2323178881@qq.com>
2021-07-30 13:51:57 +0800

    Initial commit

---
+++ html/mysql.php
@@ -0,0 +1,11 @@
+<?php 
+$dbhost = 'mysql';  // mysql服务器主机地址
+$dbuser = 'root';            // mysql用户名
+$dbpass = '123456';          // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+    die('Could not connect: ' . mysqli_error());
+}
+echo 'mysql connect success!';
+mysqli_close($conn);
